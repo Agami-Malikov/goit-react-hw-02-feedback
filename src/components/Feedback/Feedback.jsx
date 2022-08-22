2cd7fae3ea6e3f0201278250fd6b7ca76814edd1
Agami-Malikov <agami.malikov@gmail.com>
2022-08-22 17:56:56 +0300

    fix state

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,76 +1,59 @@
+import PropTypes from 'prop-types';
 import s from './feedback.module.css';
-import { Component } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 import Statistics from './Statistics/Statistics';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const Feedback = ({
+  state,
+  countTotalFeedback,
+  countPositiveFeedbackPercentage,
+  leaveVote,
+}) => {
+  const { good, neutral, bad } = state;
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
+  const total = countTotalFeedback();
 
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    if (!total) {
-      return 0;
-    }
-    const { good } = this.state;
-    const result = Math.round((good / total) * 100);
+  const positive = countPositiveFeedbackPercentage();
 
-    return result;
-  }
+  return (
+    <div className={s.feedback}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(state)}
+          onLeaveFeedback={leaveVote}
+        />
+      </Section>
 
-  leaveVote = propName => {
-    this.setState(prevState => {
-      const value = prevState[propName];
-      return {
-        [propName]: value + 1,
-      };
-    });
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    const total = this.countTotalFeedback();
-
-    const positive = this.countPositiveFeedbackPercentage();
-
-    return (
-      <div className={s.feedback}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.leaveVote}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          <div className={s.stats}>
-            {total === 0 ? (
-              <Notification message="There is no feedback" />
-            ) : (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={total}
-                positivePercentage={positive}
-              />
-            )}
-          </div>
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title="Statistics">
+        <div className={s.stats}>
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positive}
+            />
+          )}
+        </div>
+      </Section>
+    </div>
+  );
+};
 
 export default Feedback;
+
+Feedback.propTypes = {
+  state: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }),
+  countTotalFeedback: PropTypes.func.isRequired,
+  countPositiveFeedbackPercentage: PropTypes.func.isRequired,
+  leaveVote: PropTypes.func.isRequired,
+};
